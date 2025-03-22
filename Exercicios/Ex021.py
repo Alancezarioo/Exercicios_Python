242978febbb6e7dd870aaa9132067a6859d1051c
Alan Cezario Souza <alancasario09@gmail.com>
2025-03-22 13:13:42 -0300

    Py

---
+++ Exercicios/Ex021.py
@@ -0,0 +1,5 @@
+#Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+from playsound3 import playsound
+
+playsound ("/Users/alanc/OneDrive/Documentos/GitHub/Exercicios_Python/Ex021.mp3")
